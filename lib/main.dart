6d3dcdc16bcb6185edda0d27eba8292c0bf6c32b
Bikram Kumar <coolbikram0@gmail.com>
2024-06-29 20:49:41 +0530

    added favourites and ui

--- lib/main.dart
+++ lib/main.dart
@@ -3,47 +3,199 @@ import 'package:english_words/english_words.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-    runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-
         create: (context) => MyAppState(),
         child: MaterialApp(
-            title: 'Namer',
-            theme: ThemeData(
-                useMaterial3: true,
-                colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber)
-            ),
-            home: MyHomePage(),)
-        
-    );
-    }
+          title: 'Namer',
+          theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                  seedColor: Color.fromARGB(255, 0, 252, 168))),
+          home: MyHomePage(),
+        ));
+  }
 }
 
 class MyAppState extends ChangeNotifier {
-    var current = WordPair.random();
+  var current = WordPair.random();
+
+  var favIcon = Icons.favorite_border;
+
+  var favourites = <WordPair>[];
+
+  void getNext() {
+    current = WordPair.random();
+    favIcon = Icons.favorite_border;
+    notifyListeners();
+  }
+
+  void toggleFavourite() {
+    if (favourites.contains(current)) {
+      favourites.remove(current);
+      favIcon = Icons.favorite_border;
+    } else {
+      favourites.add(current);
+      favIcon = Icons.favorite;
+    }
+    notifyListeners();
+  }
 }
 
-class MyHomePage extends StatelessWidget {
- 
-    @override
-    Widget build(BuildContext context) {
-        var appState = context.watch<MyAppState>();
-        return Scaffold(
-            body: Column(
-                children: [
-                    Text('A random idea'),
-                    Text(appState.current.asLowerCase)
-                ],
-            )
-        );
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+
+    if (selectedIndex == 0) {
+      page = GeneratorPage();
+    } else {
+      page = FavouritesPage();
     }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: false,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Word(pair: pair),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavourite();
+                  },
+                  icon: Icon(appState.favIcon),
+                  label: Text("Like"),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      appState.getNext();
+                    },
+                    child: Text("Next")),
+              ],
+            ),
+          ],
+        ),
+    )
+    );
+  }
+}
+
+class Word extends StatelessWidget {
+  const Word({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          "${pair.first}${pair.second}",
+          style: style,
+        ),
+      ),
+    );
+  }
+}
+
+
+class FavouritesPage extends StatelessWidget {
+  const FavouritesPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favourites.isEmpty) {
+      return Center(
+        child: Text('No favourites yet.'),
+      );
+    }
+
+    
+    return Center(
+      child: ListView(
+        children: [
+          Padding(padding: const EdgeInsets.all(10)),
+      
+          for (WordPair wp in appState.favourites) Center(child: Text(wp.asLowerCase)),
+        ],
+      ),
+    );
+      
+  }
 }
